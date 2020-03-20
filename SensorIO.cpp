e25a36dd8519843d175506cee6a45827fe36fbf9
Lwin Maung <lwin@maungs.com>
2020-03-19 21:12:06 -0500

    initial sensor project

---
+++ SensorIO.cpp
@@ -0,0 +1,5 @@
+#include "Arduino.h"
+#include "SensorIO.h"
+
+SensorIO::SensorIO(){
+}
---
+++ SensorIO.h
@@ -0,0 +1,9 @@
+#ifndef SensorIO_h
+#define SensorIO_h
+
+class SensorIO{
+  public:
+    SensorIO();
+  private:
+};
+#endif
